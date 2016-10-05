5eb6ef5a4fcc2833337ac2145cd6c24d34f4ac15
AbeKenshi <abeken1224@yahoo.co.jp>
2016-10-06 02:25:45 +0900

    MNIST用のニューラルネットの追加

--- ch03/showMnist.py
+++ ch03/showMnist.py
@@ -10,14 +10,15 @@ def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
+
 # 最初の呼び出しは数分待つ
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 img = x_train[0]
 label = t_train[0]
-print(label) # 5
+print(label)  # 5
 
-print(img.shape)    # (784,)
-img = img.reshape(28, 28)   # 形状を元の画像サイズに変形
+print(img.shape)  # (784,)
+img = img.reshape(28, 28)  # 形状を元の画像サイズに変形
 print(img.shape)
 
 img_show(img)
